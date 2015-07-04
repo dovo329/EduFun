a954e32f2bb91129be058babf40db1917bb85d7f
Doug Voss <douglas.c.voss@gmail.com>
2015-07-03 19:52:42 -0600

    compresses png files

---
+++ Art/compressPngCards.sh
@@ -0,0 +1,9 @@
+convert CardBack.png CardBack.png
+convert PenguinCard.png PenguinCard.png
+convert CatCard.png CatCard.png
+convert CarCard.png CarCard.png
+convert RainbowCard.png RainbowCard.png
+convert StarCard.png StarCard.png
+convert IceCreamCard.png IceCreamCard.png
+convert FlowerCard.png FlowerCard.png
+convert BearCard.png BearCard.png
---
+++ Art/compressPngs.sh
@@ -0,0 +1,16 @@
+convert CardBack.png CardBack.png
+convert PenguinCard.png PenguinCard.png
+convert CatCard.png CatCard.png
+convert CarCard.png CarCard.png
+convert RainbowCard.png RainbowCard.png
+convert StarCard.png StarCard.png
+convert IceCreamCard.png IceCreamCard.png
+convert FlowerCard.png FlowerCard.png
+convert BearCard.png BearCard.png
+convert Camera.png
+convert Confetti.png
+convert ConfettiCell.png
+convert Next.png
+convert Previous.png
+convert PaintersPalette.png
+convert Zoom.png
